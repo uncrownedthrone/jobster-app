becd6daf63ef7850d79e33bce3f15b687eebc102
Justin Swiger <justin.swiger@me.com>
2022-05-24 15:15:14 -0400

    completed login/register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import { Landing, Error, Dashboard, Register } from './Pages'
+import { Landing, Error, Dashboard, Register } from './pages'
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
       </Routes>
+      <ToastContainer position='top-center' />
     </BrowserRouter>
   )
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,7 +1,99 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { toast } from 'react-toastify'
+
+import { Logo, FormRow } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+import { loginUser, registerUser } from '../features/user/userSlice'
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
 
 const Register = () => {
-  return <div>Register</div>
+  const [values, setValues] = useState(initialState)
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setValues({ ...values, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('please fill out all fields')
+      return
+    }
+    if (isMember) {
+      dispatch(loginUser({ email, password }))
+      return
+    }
+    dispatch(registerUser({ name, email, password }))
+  }
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember })
+  }
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/')
+      }, 2000)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user])
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {/* name field */}
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            name='name'
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        {/* email field */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          {isLoading ? 'loading...' : 'submit'}
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  )
 }
 
 export default Register
