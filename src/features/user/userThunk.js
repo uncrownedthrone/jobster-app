3c9e2af57f73fdc9cd26fa4bf19f964f98733bc6
Justin Swiger <justin.swiger@me.com>
2022-05-30 11:01:24 -0400

    completed profile page

---
+++ src/features/user/userThunk.js
@@ -0,0 +1,34 @@
+import customFetch from '../../utils/axios'
+import { logoutUser } from './userSlice'
+
+export const registerUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user)
+    return resp.data
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg)
+  }
+}
+
+export const loginUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user)
+    return resp.data
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg)
+  }
+}
+
+export const updateUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.patch(url, user, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    })
+    return resp.data
+  } catch (error) {
+    console.log(error.response)
+    return thunkAPI.rejectWithValue(error.response.data.msg)
+  }
+}
